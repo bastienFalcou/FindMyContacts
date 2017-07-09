281de6b5647887f6cc61f45bd29fb9b311874054
Bastien Falcou <bastien@fueled.com>
2017-07-09 01:04:42 +0100

    fix(EntranceViewModel): fix performance issue due to data source update performed too often

--- ContactSyncing/Controllers/EntranceViewModel.swift
+++ ContactSyncing/Controllers/EntranceViewModel.swift
@@ -26,21 +26,13 @@ final class EntranceViewModel: NSObject {
 		super.init()
 
 		self.disposable += self.isSyncing <~ ContactFetcher.shared.syncContactsAction.isExecuting
-		self.disposable += self.syncedPhoneContacts.producer.startWithValues { [weak self] syncedPhoneContacts in
-			let sections: [DataSourceSection<ContactTableViewCellModel>] = syncedPhoneContacts
-				.splitBetween {
-					return floor($0.0.dateAdded.timeIntervalSince1970 / (60 * 60 * 24)) != floor($0.1.dateAdded.timeIntervalSince1970 / (60 * 60 * 24))
-				}.map { contacts in
-					return DataSourceSection(items: contacts.map { ContactTableViewCellModel(contact: $0) })
-			}
-			self?.dataSource.value = StaticDataSource(sections: sections)
-		}
 	}
 
 	func syncContacts() {
 		self.disposable += ContactFetcher.shared.syncContactsAction.apply().startWithResult { [weak self] result in
 			if let syncedContacts = result.value {
 				syncedContacts.forEach { self?.syncedPhoneContacts.value.insert($0) }
+				self?.updateDataSource()
 			}
 		}
 	}
@@ -50,9 +42,20 @@ final class EntranceViewModel: NSObject {
 			try RealmManager.shared.realm.write {
 				RealmManager.shared.realm.delete(RealmManager.shared.realm.objects(PhoneContact.self))
 				self.syncedPhoneContacts.value = Set(PhoneContact.allPhoneContacts())
+				self.updateDataSource()
 			}
 		} catch {
 			print("Entrance View Model: could not remove all contacts")
 		}
+	}
+
+	fileprivate func updateDataSource() {
+		let sections: [DataSourceSection<ContactTableViewCellModel>] = self.syncedPhoneContacts.value
+			.splitBetween {
+				return floor($0.0.dateAdded.timeIntervalSince1970 / (60 * 60 * 24)) != floor($0.1.dateAdded.timeIntervalSince1970 / (60 * 60 * 24))
+			}.map { contacts in
+				return DataSourceSection(items: contacts.map { ContactTableViewCellModel(contact: $0) })
+		}
+		self.dataSource.value = StaticDataSource(sections: sections)
 	}
 }
